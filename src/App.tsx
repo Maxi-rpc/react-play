677c8423536534b1e53a2ff14028f5564272da14
Maximiliano <maxirpc2607@gmail.com>
2025-03-06 23:57:02 -0300

    add tailwind base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css';
+import './index.css';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div className="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+      <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
+      <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer
+        space.
+      </p>
     </div>
   );
 };
