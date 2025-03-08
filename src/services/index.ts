cc73778a911b745fda773b4110de81e5f623f211
Maximiliano <maxirpc2607@gmail.com>
2025-03-08 18:16:31 -0300

    update data
    se agregan todos los nuevos play

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,78 +1,488 @@
+const BaseUrl = 'https://streamdz4.lat/player/embed.php?channel=';
+
 export const Data = [
   {
     name: 'espn',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn',
+    url: `${BaseUrl}espn`,
   },
   {
     name: 'espn2',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn2',
+    url: `${BaseUrl}espn2`,
   },
   {
     name: 'espn3',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn3',
+    url: `${BaseUrl}espn3`,
   },
   {
     name: 'espn4',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn4',
+    url: `${BaseUrl}espn4`,
   },
   {
     name: 'espn5',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn5',
+    url: `${BaseUrl}espn5`,
   },
   {
     name: 'espn6',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn6',
+    url: `${BaseUrl}espn6`,
   },
   {
     name: 'espn7',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn7',
+    url: `${BaseUrl}espn7`,
   },
   {
     name: 'espnusa',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnusa',
+    url: `${BaseUrl}espnusa`,
   },
   {
     name: 'espnbr',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnbr',
+    url: `${BaseUrl}espnbr`,
   },
   {
     name: 'espnbr2',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnbr2',
+    url: `${BaseUrl}espnbr2`,
   },
   {
     name: 'espnbr3',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnbr3',
+    url: `${BaseUrl}espnbr3`,
   },
   {
     name: 'espnnl',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnnl',
+    url: `${BaseUrl}espnnl`,
   },
   {
     name: 'espn2nl',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn2nl',
+    url: `${BaseUrl}espn2nl`,
   },
   {
     name: 'espn3nl',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espn3nl',
+    url: `${BaseUrl}espn3nl`,
   },
   {
     name: 'foxar',
-    url: 'https://streamdz4.lat/player/embed.php?channel=foxar',
+    url: `${BaseUrl}foxar`,
   },
   {
     name: 'fox2ar',
-    url: 'https://streamdz4.lat/player/embed.php?channel=fox2ar',
+    url: `${BaseUrl}fox2ar`,
   },
   {
     name: 'fox3ar',
-    url: 'https://streamdz4.lat/player/embed.php?channel=fox3ar',
+    url: `${BaseUrl}fox3ar`,
   },
   {
     name: 'espnpremium',
-    url: 'https://streamdz4.lat/player/embed.php?channel=espnpremium',
+    url: `${BaseUrl}espnpremium`,
   },
   {
-    name: 'disney',
-    url: 'https://streamdz4.lat/player/embed.php?channel=disney2',
+    name: 'dsports',
+    url: `${BaseUrl}dsports`,
+  },
+  {
+    name: 'dsports2',
+    url: `${BaseUrl}dsports2`,
+  },
+  {
+    name: 'dsportsplus',
+    url: `${BaseUrl}dsportsplus`,
+  },
+  {
+    name: 'tntar',
+    url: `${BaseUrl}tntar`,
+  },
+  {
+    name: 'tntpremium',
+    url: `${BaseUrl}tntpremium`,
+  },
+  {
+    name: 'tyc',
+    url: `${BaseUrl}tyc`,
+  },
+  {
+    name: 'telefe',
+    url: `${BaseUrl}telefe`,
+  },
+  {
+    name: 'vtvplus',
+    url: `${BaseUrl}vtvplus`,
+  },
+  {
+    name: 'flutv',
+    url: `${BaseUrl}flutv`,
+  },
+  {
+    name: 'liga1max',
+    url: `${BaseUrl}liga1max`,
+  },
+  {
+    name: 'foxpremium',
+    url: `${BaseUrl}foxpremium`,
+  },
+  {
+    name: 'foxmx',
+    url: `${BaseUrl}foxmx`,
+  },
+  {
+    name: 'foxmx2',
+    url: `${BaseUrl}foxmx2`,
+  },
+  {
+    name: 'foxmx3',
+    url: `${BaseUrl}foxmx3`,
+  },
+  {
+    name: 'espnmx',
+    url: `${BaseUrl}espnmx`,
+  },
+  {
+    name: 'espnmx2',
+    url: `${BaseUrl}espnmx2`,
+  },
+  {
+    name: 'espn3mx',
+    url: `${BaseUrl}espn3mx`,
+  },
+  {
+    name: 'espn4mx',
+    url: `${BaseUrl}espn4mx`,
+  },
+  {
+    name: 'tvcdeportes',
+    url: `${BaseUrl}tvcdeportes`,
+  },
+  {
+    name: 'azteca7',
+    url: `${BaseUrl}azteca7`,
+  },
+  {
+    name: 'aztecadeportes',
+    url: `${BaseUrl}aztecadeportes`,
+  },
+  {
+    name: 'deportestvc Eventos',
+    url: `${BaseUrl}deportestvc`,
+  },
+  {
+    name: 'tudnusa',
+    url: `${BaseUrl}tudnusa`,
+  },
+  {
+    name: 'tudnmx',
+    url: `${BaseUrl}tudnmx`,
+  },
+  {
+    name: 'skymx',
+    url: `${BaseUrl}skymx`,
+  },
+  {
+    name: 'canal5mx',
+    url: `${BaseUrl}canal5mx`,
+  },
+  {
+    name: 'universo',
+    url: `${BaseUrl}universo`,
+  },
+  {
+    name: 'telemundo',
+    url: `${BaseUrl}telemundo`,
+  },
+  {
+    name: 'foxdeportes',
+    url: `${BaseUrl}foxdeportes`,
+  },
+  {
+    name: 'unimas',
+    url: `${BaseUrl}unimas`,
+  },
+  {
+    name: 'espndeportes',
+    url: `${BaseUrl}espndeportes`,
+  },
+  {
+    name: 'winsportsplus',
+    url: `${BaseUrl}winsportsplus`,
+  },
+  {
+    name: 'winsports',
+    url: `${BaseUrl}winsports`,
+  },
+  {
+    name: 'sportv1',
+    url: `${BaseUrl}sportv1`,
+  },
+  {
+    name: 'sportv2',
+    url: `${BaseUrl}sportv2`,
+  },
+  {
+    name: 'sportv3',
+    url: `${BaseUrl}sportv3`,
+  },
+  {
+    name: 'sportv4',
+    url: `${BaseUrl}sportv4`,
+  },
+  {
+    name: 'sportv5',
+    url: `${BaseUrl}sportv5`,
+  },
+  {
+    name: 'sportv6',
+    url: `${BaseUrl}sportv6`,
+  },
+  {
+    name: 'dazn1es',
+    url: `${BaseUrl}dazn1es`,
+  },
+  {
+    name: 'dazn2es',
+    url: `${BaseUrl}dazn2es`,
+  },
+  {
+    name: 'dazn3es',
+    url: `${BaseUrl}dazn3es`,
+  },
+  {
+    name: 'dazn4es',
+    url: `${BaseUrl}dazn4es`,
+  },
+  {
+    name: 'daznf1',
+    url: `${BaseUrl}daznf1`,
+  },
+  {
+    name: 'dazn1pt',
+    url: `${BaseUrl}dazn1pt`,
+  },
+  {
+    name: 'dazn2pt',
+    url: `${BaseUrl}dazn2pt`,
+  },
+  {
+    name: 'dazn3pt',
+    url: `${BaseUrl}dazn3pt`,
+  },
+  {
+    name: 'dazn4pt',
+    url: `${BaseUrl}dazn4pt`,
+  },
+  {
+    name: 'dazn5pt',
+    url: `${BaseUrl}dazn5pt`,
+  },
+  {
+    name: 'canal11pt',
+    url: `${BaseUrl}canal11pt`,
+  },
+  {
+    name: 'premiere',
+    url: `${BaseUrl}premiere`,
+  },
+  {
+    name: 'premiere2',
+    url: `${BaseUrl}premiere2`,
+  },
+  {
+    name: 'premiere3',
+    url: `${BaseUrl}premiere3`,
+  },
+  {
+    name: 'premiere4',
+    url: `${BaseUrl}premiere4`,
+  },
+  {
+    name: 'sportvbr1',
+    url: `${BaseUrl}sportvbr1`,
+  },
+  {
+    name: 'sportvbr2',
+    url: `${BaseUrl}sportvbr2`,
+  },
+  {
+    name: 'skysport1at',
+    url: `${BaseUrl}skysport1at`,
+  },
+  {
+    name: 'skybundes1',
+    url: `${BaseUrl}skybundes1`,
+  },
+  {
+    name: 'skybundes2',
+    url: `${BaseUrl}skybundes2`,
+  },
+  {
+    name: 'skybundes3',
+    url: `${BaseUrl}skybundes3`,
+  },
+  {
+    name: 'skybundes4',
+    url: `${BaseUrl}skybundes4`,
+  },
+  {
+    name: 'skybundes5',
+    url: `${BaseUrl}skybundes5`,
+  },
+  {
+    name: 'ziggo',
+    url: `${BaseUrl}ziggo`,
+  },
+  {
+    name: 'foxplus',
+    url: `${BaseUrl}foxplus`,
+  },
+  {
+    name: 'fox1usa',
+    url: `${BaseUrl}fox1usa`,
+  },
+  {
+    name: 'fox2usa',
+    url: `${BaseUrl}fox2usa`,
+  },
+  {
+    name: 'tntuk',
+    url: `${BaseUrl}tntuk`,
+  },
+  {
+    name: 'tnt2uk',
+    url: `${BaseUrl}tnt2uk`,
+  },
+  {
+    name: 'tnt3uk',
+    url: `${BaseUrl}tnt3uk`,
+  },
+  {
+    name: 'tnt4uk',
+    url: `${BaseUrl}tnt4uk`,
+  },
+  {
+    name: 'tnt5uk',
+    url: `${BaseUrl}tnt5uk`,
+  },
+  {
+    name: 'usanetwork',
+    url: `${BaseUrl}usanetwork`,
+  },
+  {
+    name: 'skycalcio',
+    url: `${BaseUrl}skycalcio`,
+  },
+  {
+    name: 'laligauk',
+    url: `${BaseUrl}laligauk`,
+  },
+  {
+    name: 'skymain',
+    url: `${BaseUrl}skymain`,
+  },
+  {
+    name: 'skyfootball',
+    url: `${BaseUrl}skyfootball`,
+  },
+  {
+    name: 'skypremiere',
+    url: `${BaseUrl}skypremiere`,
+  },
+  {
+    name: 'skytenis',
+    url: `${BaseUrl}skytenis`,
+  },
+  {
+    name: 'skycricket',
+    url: `${BaseUrl}skycricket`,
+  },
+  {
+    name: 'skygolf',
+    url: `${BaseUrl}skygolf`,
+  },
+  {
+    name: 'skyf1',
+    url: `${BaseUrl}skyf1`,
+  },
+  {
+    name: 'premiersports1',
+    url: `${BaseUrl}premiersports1`,
+  },
+  {
+    name: 'premiersports2',
+    url: `${BaseUrl}premiersports2`,
+  },
+  {
+    name: 'go3',
+    url: `${BaseUrl}go3`,
+  },
+  {
+    name: 'tsn1',
+    url: `${BaseUrl}tsn1`,
+  },
+  {
+    name: 'tsn2',
+    url: `${BaseUrl}tsn2`,
+  },
+  {
+    name: 'tsn3',
+    url: `${BaseUrl}tsn3`,
+  },
+  {
+    name: 'tsn4',
+    url: `${BaseUrl}tsn4`,
+  },
+  {
+    name: 'tsn5',
+    url: `${BaseUrl}tsn5`,
+  },
+  {
+    name: 'sportneteast',
+    url: `${BaseUrl}sportneteast`,
+  },
+  {
+    name: 'sn360',
+    url: `${BaseUrl}sn360`,
+  },
+  {
+    name: 'beinsportsextran',
+    url: `${BaseUrl}beinsportsextran`,
+  },
+  {
+    name: 'beinsportsn',
+    url: `${BaseUrl}beinsportsn`,
+  },
+  {
+    name: 'disney1',
+    url: `${BaseUrl}disney1`,
+  },
+  {
+    name: 'disney2',
+    url: `${BaseUrl}disney2`,
+  },
+  {
+    name: 'disney3',
+    url: `${BaseUrl}disney3`,
+  },
+  {
+    name: 'disney4',
+    url: `${BaseUrl}disney4`,
+  },
+  {
+    name: 'disney5',
+    url: `${BaseUrl}disney5`,
+  },
+  {
+    name: 'disney6',
+    url: `${BaseUrl}disney6`,
+  },
+  {
+    name: 'disney7',
+    url: `${BaseUrl}disney7`,
+  },
+  {
+    name: 'disney8',
+    url: `${BaseUrl}disney8`,
+  },
+  {
+    name: 'disney9',
+    url: `${BaseUrl}disney9`,
+  },
+  {
+    name: 'disney10',
+    url: `${BaseUrl}disney10`,
   },
 ];
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter, CardTitle } from '../../components';
+import { Card, CardContent } from '../../components';
 import { Data } from '../../services';
 
 const HomePage = () => {
@@ -8,23 +8,20 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="grid grid-cols-12 grid-rows-5 gap-4 px-4 py-4">
+      <div className="grid grid-cols-6 xl:grid-cols-12 grid-rows-5 gap-4 px-4 py-4">
         {Data.map((item, index) => (
-          <div key={index} className="col-span-2 row-span-1">
+          <div key={index} className="col-span-2 xl:col-span-2 row-span-1">
             <Card>
               <CardContent>
-                <CardTitle title={item.name} />
-              </CardContent>
-              <CardFooter>
                 <a
                   href={item.url}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="bg-[#7e22ce] text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform text-center"
                 >
-                  Go Play
+                  {item.name}
                 </a>
-              </CardFooter>
+              </CardContent>
             </Card>
           </div>
         ))}
